5a520678bf9181575df228429a5f64b279f60dcc
nahuel lavena <nahuelito_2000@hotmail.com>
2022-08-23 22:19:45 -0300

    Agregando archivos portafolio

---
+++ js/index.js
@@ -0,0 +1,23 @@
+window.sr =ScrollReveal();
+
+sr.reveal( '.navbar' ,{
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+
+});
+
+sr.reveal( '.scroll-banner' ,{
+    duration: 5000,
+    origin: 'bottom',
+    distance: '-100px'
+
+});
+
+sr.reveal( '.scroll-about' ,{
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+
+});
+
